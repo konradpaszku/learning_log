1a752889f853b7ec1f929385edc2e48421cc4cc0
konradpaszku <konrad.paszek@icloud.com>
2020-10-25 13:00:23 +0100

    Change function in views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, Http404
 from .models import Topic, Entry
@@ -25,7 +25,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Wyswietla pojedynczy temat i wszystkie powiazane z nim wpisy."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     #Make sure that topic is being fot actual user
     check_topic_owner(request, topic_id)
     entries = topic.entry_set.order_by('-date_added')
